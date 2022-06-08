39047c33227bcff4d0074b8f59ec47e18c058d76
jmainsworth <107142375+jmainsworth@users.noreply.github.com>
2022-06-08 12:35:17 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- 
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="shortcut icon" href="">
+	--> 
+
+	<title>PREM 2021</title>
+
+	<!-- Need to do the style in css (it will go here) -->
+	<!--
+	<link rel="stylesheet" href="css/style.css">
+	-->
+
+	<!-- Need to put in the website elements -->
+
+</head>
+
+
+<body>
+	<?php include "header.html" ?>
+
+	<div class="container" style="background:white">
+		<!-- What do I need to put on the homepage? -->
+		<!-- <> -->
+
+
+	<!-- This is where the Footer goes if wanted. 
+	<?php include "footer.html" ?>
+	-->
+
+</body>
